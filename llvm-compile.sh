4b01ceeec9caf63b96cda53cd81a6d4c6e9265c8
Ryan Williams <ryan.rpwilliams@gmail.com>
2020-03-23 15:41:59 -0400

    Automated compiling all LLVM IR for testing

---
+++ llvm-compile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Compile all the subdirs applicable code to LLVM IR.
+# lib/ will have errors, but are safe to ignore for our purposes.
+DIR=$PWD
+
+make binary -j
+(cd src ; make -j) || exit 1
+
+cd client ; make -j
+chmod +x mosquitto_sub
+chmod +x mosquitto_pub
